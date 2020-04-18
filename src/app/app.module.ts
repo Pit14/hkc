3c9c54bccc2f6a369165a4fc1fa8857722d46cd3
Thomas Edgar <ethomas@ecole.ensicaen.fr>
2020-04-18 02:15:43 +0200

    Small modifications in order to include the project to my website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {MatSelectModule} from '@angular/material/select';
 
 
-import { AppComponent } from './app.component';
-import { NailComponent } from './nail/nail.component';
-import { CharmComponent } from './charm/charm.component';
-import { CalculatorComponent } from './calculator/calculator.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {NailComponent} from './nail/nail.component';
+import {CharmComponent} from './charm/charm.component';
+import {CalculatorComponent} from './calculator/calculator.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { NgSelectModule } from '@ng-select/ng-select';
-import { HeaderComponent } from './header/header.component';
-import { TestComponent } from './test/test.component';
-import { DamageTableComponent } from './damage-table/damage-table.component';
-import { LifeMaskComponent } from './life-mask/life-mask.component';
-import { CharmsEquippedComponent } from './charms-equiped/charms-equipped.component';
-import { NotchesComponent } from './notches/notches.component';
-import { ActiveEffectsComponent } from './active-effects/active-effects.component';
+import {NgSelectModule} from '@ng-select/ng-select';
+import {HeaderComponent} from './header/header.component';
+import {TestComponent} from './test/test.component';
+import {DamageTableComponent} from './damage-table/damage-table.component';
+import {LifeMaskComponent} from './life-mask/life-mask.component';
+import {CharmsEquippedComponent} from './charms-equiped/charms-equipped.component';
+import {NotchesComponent} from './notches/notches.component';
+import {ActiveEffectsComponent} from './active-effects/active-effects.component';
+
 
 @NgModule({
   declarations: [
@@ -39,7 +40,7 @@ import { ActiveEffectsComponent } from './active-effects/active-effects.componen
     NgSelectModule,
     BrowserAnimationsModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
